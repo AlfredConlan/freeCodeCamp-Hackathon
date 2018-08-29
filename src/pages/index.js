200d28eb98c939e3785ec2caed4f58e08510a0d1
root <root@localhost.localdomain>
2018-08-29 15:18:16 -0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi People!</h1>
+    <h2>My name is Alfred and I'm honored to meet you!</h2>
+    <h3>I hope together we can build something great!</h3>
+  </div>
+)
+
+export default IndexPage
